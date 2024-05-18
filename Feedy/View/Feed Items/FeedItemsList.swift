7dbd3ef5bd440573b9fbbe5ad8942963d0bf5c6a
Ivana <ivana.rast@gmail.com>
2024-05-19 01:09:12 +0200

    fix: using FeedtemCell

--- Feedy/View/Feed Items/FeedItemsList.swift
+++ Feedy/View/Feed Items/FeedItemsList.swift
@@ -13,30 +13,12 @@ struct FeedItemsList: View {
     
     var body: some View {
         List {
-            ForEach(feed.items) { feedItem in
+            ForEach(feed.items, id: \.id) { feedItem in
                 ZStack(alignment: .leading) {
                     NavigationLink(destination: FeedItemDetails(feedItem: feedItem)) {
                         EmptyView()
                     }.opacity(0)
-                    HStack {
-                        if let imageURL = feedItem.imageURL {
-                            AsyncImage(url: imageURL) { image in
-                                image.resizable().aspectRatio(contentMode: .fit)
-                            } placeholder: {
-                                ProgressView()
-                            }
-                            .frame(width: 30, height: 30)
-                        } else {
-                            let logoURL = Bundle.main.url(forResource: "rss-logo", withExtension: "svg")!
-                            SVGView(contentsOf: logoURL).frame(width: 30, height: 30)
-                        }
-                        VStack(alignment: .leading) {
-                            Text(feedItem.title).font(.headline)
-                            if let details = feedItem.details {
-                                Text(details).font(.subheadline)
-                            }
-                        }
-                    }
+                    FeedItemCell(feedItem: feedItem)
                 }
             }
         }
